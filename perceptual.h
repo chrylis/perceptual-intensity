6416a8c72dcb9f8f858ab79fe7bac858946bd057
Christopher Smith <chrylis@gmail.com>
2017-10-23 02:30:41 -0500

    initial checkin of basic perceptual tables

---
+++ perceptual.h
@@ -0,0 +1,9 @@
+#ifndef PERCEPTUAL_H
+#define PERCEPTUAL_H
+
+#include <cstdint>
+
+extern "C" const uint8_t ABSOLUTE_TO_PERCEPTUAL[];
+extern "C" const uint8_t PERCEPTUAL_TO_ABSOLUTE[];
+
+#endif // PERCEPTUAL_H
